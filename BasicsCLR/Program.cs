e7ae4df3dc0fbc70f4ba2dbaeeb4fe11085661e7
Анна Башкатова <annabashk13@gmail.com>
2022-08-28 14:26:50 +0300

    Применение конструкции Switch

--- BasicsCLR/Program.cs
+++ BasicsCLR/Program.cs
@@ -6,32 +6,34 @@ namespace BasicsCLR
     {
         static void Main(string[] args)
         {
-			Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы:");
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы:");
 
-			var color = Console.ReadLine();
+            var color = Console.ReadLine();
 
-			if (color == "red")
-			{
-				Console.BackgroundColor = ConsoleColor.Red;
-				Console.ForegroundColor = ConsoleColor.Black;
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
 
-				Console.WriteLine("Your color is red!");
-			}
+                    Console.WriteLine("Your color is red!");
+                    break;
 
-			else if (color == "green")
-			{
-				Console.BackgroundColor = ConsoleColor.Green;
-				Console.ForegroundColor = ConsoleColor.Black;
 
-				Console.WriteLine("Your color is green!");
-			}
-			else
-			{
-				Console.BackgroundColor = ConsoleColor.Cyan;
-				Console.ForegroundColor = ConsoleColor.Black;
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
 
-				Console.WriteLine("Your color is cyan!");
-			}
-		}
+                    Console.WriteLine("Your color is green!");
+                    break;
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+            }
+        }
     }
 }
